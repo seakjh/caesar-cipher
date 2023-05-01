85cc9f99593c27054b20fe9d005f25ddf815876f
seakjh <skykjh0513@gmail.com>
2023-05-01 18:38:56 +0900

    feat: Write Caesar cipher code

--- caesar.py
+++ caesar.py
@@ -1,1 +1,11 @@
-print('It works')
+import string
+
+def caesar(text, num):
+    cipher = str.maketrans(string.ascii_lowercase, string.ascii_lowercase[num:] + string.ascii_lowercase[:num])
+    return text.translate(cipher)
+
+if __name__ == '__main__':
+    text = str(input('Enter text: '))
+    num = int(input('Enter Key num: '))
+    print('encrypted text : ' + caesar(text, num))
+    
